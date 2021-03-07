772c55018d27b39039a290e43b1b6b5c98050ea9
Krystian Kopeć <73670385+Cannibal90@users.noreply.github.com>
2021-03-08 00:40:29 +0100

    init commit

---
+++ backend/src/test/java/com/projekt/cannibal/car_rent/CarRentApplicationTests.java
@@ -0,0 +1,11 @@
+package com.projekt.cannibal.car_rent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
