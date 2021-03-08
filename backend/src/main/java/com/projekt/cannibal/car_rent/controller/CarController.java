ec643024778c7fbf0cfd4a54a4eec92501597cff
Krystian Kopeć <73670385+Cannibal90@users.noreply.github.com>
2021-03-08 14:49:33 +0100

    add controllers annotations

--- backend/src/main/java/com/projekt/cannibal/car_rent/controller/CarController.java
+++ backend/src/main/java/com/projekt/cannibal/car_rent/controller/CarController.java
@@ -1,4 +1,9 @@
 package com.projekt.cannibal.car_rent.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/car")
 public class CarController {
 }
