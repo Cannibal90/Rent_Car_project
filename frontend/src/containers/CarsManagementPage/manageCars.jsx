bcafd3d360d94b9afa2c7b981a520427c5d24b38
Krystian Kopeć <73670385+Cannibal90@users.noreply.github.com>
2021-04-03 22:27:06 +0200

    start car manmagement

--- frontend/src/containers/CarsManagementPage/manageCars.jsx
+++ frontend/src/containers/CarsManagementPage/manageCars.jsx
@@ -1,6 +1,7 @@
 import { ContentContainer, Title } from "../../components/Cards";
 import styled from "styled-components";
-
+import { Button } from "../../components/button";
+import { Cars } from "../../containers/OffertsPage/cars";
 const CarsContainer = styled.div`
   width: 100%;
   display: flex;
@@ -15,6 +16,12 @@ export function ManageCars(props) {
       <ContentContainer>
         <Title>Cars</Title>
       </ContentContainer>
+      <ContentContainer>
+        <Button size={25} paddingH={3} paddingW={2}>
+          Delete Mode
+        </Button>
+      </ContentContainer>
+      <Cars />
     </CarsContainer>
   );
 }
